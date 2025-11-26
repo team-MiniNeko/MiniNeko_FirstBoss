038d8350dd57d7a19466b41b420edfddfe31b239
osch7497 <osch7497@gmail.com>
2025-11-26 11:36:55 +0900

    오류때문에 커밋함
    ㅈㄱㄴ

--- Assets/1_Scripts/Players/PlayerHealth.cs
+++ Assets/1_Scripts/Players/PlayerHealth.cs
@@ -40,7 +40,7 @@ public class PlayerHealth : MonoBehaviour
     float lastAutoHealTime;
     void Start()
     {
-        maxHp = PlayerStatsManager.Instance.playerHp;
+        
         lastAutoHealTime = Time.time;
         StPos = new Vector2(transform.position.x,transform.position.y);
         _curHp = maxHp;
