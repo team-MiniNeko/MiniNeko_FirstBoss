96884efdd8a21e4e866e5c9056bc1f5f85e28e1a
kingjangjo <araloveasalll@gmail.com>
2025-12-05 02:24:47 +0900

    BigDroneAttck

--- Assets/1_Scripts/BigDrone.cs
+++ Assets/1_Scripts/BigDrone.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class BigDrone : MonoBehaviour
@@ -9,9 +10,13 @@ public class BigDrone : MonoBehaviour
     public SpriteRenderer Sprite;
     public GameObject Ammo;
     public GameObject target;
+    public GameObject explosionAttackRange;
+    public GameObject explosionAttack;
     void Start()
     {
         target = GameObject.FindWithTag("Player");
+        IEnumerator co = AttackPatterns();
+        StartCoroutine(co);
     }
     public void MoveTo(Transform T)
     { //대충 타겟쪽으로 움직이는 코드
@@ -24,10 +29,27 @@ public class BigDrone : MonoBehaviour
     }
     IEnumerator AttackPatterns()
     {
-        yield return null;
+        while (true)
+        {
+            MoveTo(target.transform);
+            Vector3 direction = target.transform.position - transform.position;
+            yield return new WaitForSeconds(0.001f);
+
+            if (Math.Abs(target.transform.position.x - transform.position.x) <= 10f)
+            { // 사거리 내로 타겟이 들어오면
+                Vector3 vec = target.transform.position;
+                var attackRange = Instantiate(explosionAttackRange, vec, Quaternion.identity);
+                yield return new WaitForSeconds(1f);
+                Destroy(attackRange );
+                var attack = Instantiate(explosionAttack, vec, Quaternion.identity);
+                yield return new WaitForSeconds(0.5f);
+                Destroy(attack );
+                yield return new WaitForSeconds(2f);
+            }
+        }
     }
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
         MoveTo(target.transform);
         if (target.transform.position.x < transform.position.x)
